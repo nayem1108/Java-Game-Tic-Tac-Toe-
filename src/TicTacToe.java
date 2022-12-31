f6f265dad4fe74f04805dd56b23e95e2d7022a2b
Nayem Uddin <nayemuddincucse@gmail.com>
2022-12-31 20:33:26 +0600

    First commit on tic-tac-toe games

---
+++ src/TicTacToe.java
@@ -0,0 +1,172 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class TicTacToe {
+    private static final char EMPTY_BOX = ' ';
+    private static final char PLAYER_ONES_SYMBOL = 'X';
+    private static final char PLAYER_TWOS_SYMBOL = '0';
+
+    private final char[][] gameBoard = new char[3][3];
+
+    private final Scanner input = new Scanner(System.in);
+
+    private String playerOne;
+    private String playerTwo;
+    private String currentPlayer;
+    private String whoWonTheGame;
+
+    public void startGame(){
+        initializeBoard();
+        askForUserName();
+
+        while(isGameNotOver()){
+            drawBoard();
+            printPlayerTurn();
+            askForManeuver();
+        }
+        printGameOver();
+    }
+
+    private void initializeBoard(){
+        for(char[] chars : gameBoard){
+            Arrays.fill(chars, EMPTY_BOX);
+        }
+    }
+
+    private void askForUserName(){
+        System.out.println("Welcome to Tic Tac Toe Game. Developed by:- Nayem Uddin");
+        System.out.println("Hey Dear, what's your name?");
+        playerOne = input.nextLine();
+        System.out.println("Who are you playing with?");
+        playerTwo = input.nextLine();
+        System.out.println("Who will playing first? press" +
+                "\n1 for " + playerOne + "\n2 for " + playerTwo);
+        int player = input.nextInt();
+        currentPlayer = player == 1 ? playerOne : playerTwo;
+    }
+
+    private boolean isGameNotOver(){
+        return !(isBoardIsFull() || hasAnyPlayerWon());
+    }
+
+    private void drawBoard(){
+        System.out.println("|---|---|---");
+        for(char[] chars : gameBoard){
+            System.out.printf("| %c | %c | %c |\n", chars[0], chars[1], chars[2]);
+        }
+        System.out.println("|---|---|---");
+    }
+
+    private void printPlayerTurn(){
+        System.out.println(whoIsPlaying() + "'s turn");
+    }
+
+    private void askForManeuver(){
+        int row;
+        int col;
+
+        do{
+            System.out.println("Enter row number(0, 1, or 2): ");
+            row = input.nextInt();
+            System.out.println("Enter col number(0, 1, or 2): ");
+            col = input.nextInt();
+        }while(!validateInput(row, col));
+
+        if(whoIsPlaying().equals(playerOne)){
+            gameBoard[row][col] = PLAYER_ONES_SYMBOL;
+            currentPlayer = playerTwo;
+        } else{
+            gameBoard[row][col] = PLAYER_TWOS_SYMBOL;
+            currentPlayer = playerOne;
+        }
+    }
+
+    private void printGameOver(){
+        drawBoard();
+        System.out.println("\uD83C \uDFAE Game Over! \uD83C \uDFAE");
+
+        if(whoWonTheGame != null){
+            System.out.println(whoWonTheGame + "won the game, " +
+                    "Congratulations ! \uD83C\uDF8A \uD83C\uDF89");
+        } else{
+            System.out.println("It's a tie! Play Again!");
+        }
+    }
+
+    private boolean isBoardIsFull(){
+        boolean result = true;
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                if(gameBoard[i][j] == EMPTY_BOX){
+                    result = false;
+                    break;
+                }
+            }
+            if(!result)
+                break;
+        }
+        return result;
+    }
+
+    private boolean hasAnyPlayerWon(){
+        char cross = ' ';
+
+//        check each row
+        for(int i = 0; i < 3; i++){
+            if(gameBoard[i][0] == gameBoard[i][1] &&
+                    gameBoard[i][1] == gameBoard[i][2] && gameBoard[i][0] != EMPTY_BOX)
+            {
+                cross = gameBoard[i][0];
+            }
+        }
+
+//        check for each column
+        for(int j = 0; j < 3; j++){
+            if(gameBoard[0][j] == gameBoard[1][j] &&
+                    gameBoard[1][j] == gameBoard[2][j] && gameBoard[0][j] != EMPTY_BOX)
+            {
+                cross = gameBoard[0][j];
+            }
+        }
+
+//        check diagonal
+        if(gameBoard[0][0] == gameBoard[1][1] &&
+                gameBoard[1][1] == gameBoard[0][2] && gameBoard[0][2] != EMPTY_BOX)
+        {
+            cross = gameBoard[0][0];
+        }
+
+//        check reverse diagonal
+        if(gameBoard[2][0] == gameBoard[1][1] &&
+                gameBoard[1][1] == gameBoard[2][2] && gameBoard[2][2] != EMPTY_BOX){
+            cross = gameBoard[2][0];
+        }
+
+        if(cross == PLAYER_ONES_SYMBOL){
+            whoWonTheGame = playerOne;
+        }else if(cross == PLAYER_TWOS_SYMBOL){
+            whoWonTheGame = playerTwo;
+        }
+
+        return whoWonTheGame != null;
+    }
+
+    private String whoIsPlaying(){
+        return currentPlayer;
+    }
+
+    private boolean validateInput(int row, int col){
+        boolean result = false;
+        if(row < 0 || col < 0 || row > 2 || col > 2){
+            System.out.println("The position is of the bounds " +
+                    "of hte board, try again");
+        }else if(gameBoard[row][col] != EMPTY_BOX){
+            System.out.println("Opponent has already made a move " +
+                    "at this position, try again");
+        }else{
+            result = true;
+        }
+
+        return result;
+    }
+}
